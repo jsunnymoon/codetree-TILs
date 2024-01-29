1e38c53c490551516afcb18674a2aa6014318d76
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-01-29 16:51:09 +0100

    Title: 장학금, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/장학금/scholarship.py
+++ 240130/장학금/scholarship.py
@@ -2,9 +2,9 @@ m, f = input().split()
 m = int(m)
 f = int(f)
 
-if m > 90 and f >= 95:
+if m >= 90 and f >= 95:
     print(1000000)
-elif m > 90 and f >= 90:
+elif m >= 90 and f >= 90:
     print(50000)
 else:
     print(0)
