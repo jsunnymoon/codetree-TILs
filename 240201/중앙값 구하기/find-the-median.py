83e309bbc42513230864f3a9f50e38b6045b375d
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-01-31 22:21:03 +0100

    Title: 중앙값 구하기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240201/중앙값 구하기/find-the-median.py
+++ 240201/중앙값 구하기/find-the-median.py
@@ -5,5 +5,5 @@ if (a < b and a > c) or (a < c and a > b):
     print(a)
 elif (b < a and b > c) or (b < c and b > a):
     print(b)
-elif (c < a and c > b) or (c < b and c > a)
+elif (c < a and c > b) or (c < b and c > a):
     print(c)
