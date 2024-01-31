03ab6b739378a0ac923c557f1de06601880c3ca3
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-01-31 21:58:18 +0100

    Title: 코로나 메뉴얼, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240201/코로나 메뉴얼/covid-manual.py
+++ 240201/코로나 메뉴얼/covid-manual.py
@@ -11,13 +11,13 @@ p3s = str(p3[0])
 p3t = int(p3[1])
 
 if p1s == "Y" and p1t >= 37:
-    if (p2s == "Y" and p2t >= 37) or (p3s == "Y" and p3t >= 37)
+    if (p2s == "Y" and p2t >= 37) or (p3s == "Y" and p3t >= 37):
         print("E")
     else:
         print("N")
 
-elif p2s == "Y" and p2t >= 37:
-    if p3s == "Y" and p3t >= 37:
+else:
+    if (p2s == "Y" and p2t >= 37) or (p3s == "Y" and p3t >= 37):
         print("E")
     else:
         print("N")
