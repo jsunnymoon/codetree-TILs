78516ad5c4f2313c31d45851b4abf079444b4c66
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-01-31 21:53:56 +0100

    Title: 코로나 메뉴얼, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240201/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,25 @@
+p1 = input().split()
+p1s = str(p1[0])
+p1t = int(p1[1])
+
+p2 = input().split()
+p2s = str(p2[0])
+p2t = int(p2[1])
+
+p3 = input().split()
+p3s = str(p3[0])
+p3t = int(p3[1])
+
+if p1s == "Y" and p1t >= 37:
+    if p2s == "Y" and p2t >= 37:
+        print("E")
+    elif p3s == "Y" and p3t >= 37:
+        print("E")
+    else:
+        print("N")
+
+elif p2s == "Y" and p2t >= 37:
+    if p3s == "Y" and p3t >= 37:
+        print("E")
+    else:
+        print("N")
