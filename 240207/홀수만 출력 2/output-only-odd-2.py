c4273726e59bde1cc4cc64abb4fc1a054bce05f0
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-02-07 00:18:15 +0100

    Title: 홀수만 출력 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240207/홀수만 출력 2/output-only-odd-2.py
+++ 240207/홀수만 출력 2/output-only-odd-2.py
@@ -1,4 +1,4 @@
-a, b = map(int, input().split())
-for i in range(b, a-1, -1):
-    if i % 2 == 1:
-        print(i, end=" ")
+b, a = map(int, input().split())
+
+for i in range(b, a-1, -2):
+    print(i, end=" ")
