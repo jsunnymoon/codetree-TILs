05485944d476456b3e92bf5f234f41dbbaa10c62
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-02-20 17:01:36 +0100

    Title: 자연수면 출력, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/자연수면 출력/output-only-natural-number.py
+++ 240221/자연수면 출력/output-only-natural-number.py
@@ -1,8 +1,8 @@
 a, b = map(int, input().split())
 if type(a) is int:
     if a > 0:
-    for _ in range(b):
-        print(a, end="")
+        for _ in range(b):
+            print(a, end="")
     
     else:
         print(0)
