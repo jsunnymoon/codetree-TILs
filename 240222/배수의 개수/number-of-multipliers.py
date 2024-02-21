155d9cfa299718c39f1371e0e6ba7ab175a2ac84
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-02-21 19:59:07 +0100

    Title: 배수의 개수, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240222/배수의 개수/number-of-multipliers.py
+++ 240222/배수의 개수/number-of-multipliers.py
@@ -2,12 +2,9 @@ three = 0
 five = 0
 for _ in range(10):
     inp = int(input())
-    if inp % 3 == 0 and inp % 5 == 0:
+    if inp % 3 == 0:
         three += 1
-        five += 1
-    elif inp % 3 == 0:
-        three += 1
-    elif inp % 5 == 0:
+    if inp % 5 == 0:
         five += 1
     
-print(f"{three} {five}")
+print(three, five)
