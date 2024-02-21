b97df70dd0cd804a1a2cb542b7324e80ff64edcc
jsunnymoon <61963771+jsunnymoon@users.noreply.github.com>
2024-02-21 20:06:32 +0100

    Title: 청소당번 정하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240222/청소당번 정하기/cleaning-numbering.py
+++ 240222/청소당번 정하기/cleaning-numbering.py
@@ -1,21 +1,12 @@
 n = int(input())
-
-cr = 0
-cor = 0
-br = 0
+cnt2, cnt3, cnt12 = 0, 0, 0
 
 for i in range(1, n+1):
-    if i % 2 == 0:
-        if i % 12 == 0:
-            br += 1
-        elif i % 3 == 0:
-            cor += 1
-        else:
-            cr += 1
+    if i % 12 == 0:
+        cnt12 += 1
     elif i % 3 == 0:
-        if i % 12 == 0:
-            br += 1
-        else:
-            cor += 1
+        cnt3 += 1
+    elif i % 2 == 0:
+        cnt2 += 1
 
-print(cr, cor, br)
+print(cnt2, cnt3, cnt12)
